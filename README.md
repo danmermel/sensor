ba862abc14db91c6ac4e932a7cf565ce93d253c9
Daniel Mermelstein <danmermel@gmail.com>
2014-12-30 23:58:55 +0000

    adding comments and readme

---
+++ README.md
@@ -0,0 +1,17 @@
+First you have to get the RPI to control a car with a couple of motors. I explained that here:
+
+https://github.com/danmermel/web-car
+
+The next step is to wire an ultrasound obstacle detector to the arrangement.
+
+I used the HC-SR04
+
+And I followed the instructions here to do it, which I found very good.
+
+https://www.modmypi.com/blog/hc-sr04-ultrasonic-range-sensor-on-the-raspberry-pi
+
+The rest is applying a bit of coding to sense distance and turn the car away from obstacles, which is in the sensor.rb script
+
+Again, many thanks to all the people who make this possible, by posting all their helpful instructions and doing awesome stuff all over the internet.
+
+
--- sensor.rb
+++ sensor.rb
@@ -1,17 +1,14 @@
 #! /usr/bin/ruby
 
 require 'wiringpi'
-require 'io/console'
-#require 'cgi'
 
-$stdout.reopen("moto_out.txt", "w")
+# this is just to write the debug somewhere
+$stdout.reopen("moto_out.txt", "w")  
 $stderr.reopen("moto_err.txt", "w")
-
-
 
 io = WiringPi::GPIO.new
 
-direction =  ARGV[0]
+direction =  ARGV[0] # gets the direction from the command line argument
 puts direction
 
 # 0 = right fwd
@@ -43,33 +40,35 @@ def stop_all(var)  #stops everything before next manouvre
    var.write(4,LOW)
 end
 
-def move_and_check(var)
+def move_and_check(var)    #goes forward until it decides obstacle is close
+
    stop_all(var)
    var.write(3,HIGH)
    var.write(1,HIGH)
    puts "going forward"
    puts "Distance measurement in progress..."
-   distance = 100
-   while distance > 40 do
+   distance = 100   # a random big number to initialise
+   while distance > 40 do    #  40cm, depends how close you want to get
       pulse_duration = 0  # reset it
       sleep(0.1)  # wait a bit..
+
       var.write(10, HIGH)   # send pulse
       sleep(0.000001)
       var.write(10, LOW)
 
       pulse_start= Time.now
-      while var.read(11) == 0 do
+      while var.read(11) == 0 do    #echo not started
          pulse_start = Time.now
       end
 
-      while var.read(11) == 1 do
+      while var.read(11) == 1 do   #echo started.. waiting for return sound
          pulse_end = Time.now
       end
 
       pulse_duration = pulse_end - pulse_start
       puts "pulse duration is " + pulse_duration.to_s
 
-      distance = pulse_duration * 17150
+      distance = pulse_duration * 17150  #in cm. using speed = distance /time
       puts "Distance is " + distance.to_s
    end  #while
    return  # will exit loop when distance is too short
@@ -79,17 +78,19 @@ def find_new_dir(var)
 # find a new position to go forward by rotating slowly and checking again
 
    stop_all(var)
-   var.write(3,HIGH)
-   sleep(0.5)  
-   return
+   var.write(3,HIGH)  # only use one wheel, hence turn
+   sleep(0.5)  # wait for some time, keep turning
+   return  # goes back and starts to move forward again
 
 end
+
+# This is where the actual script starts
 
 if direction == "[fwd]" then  #forward
    io.write(10, LOW)
    puts "waiting for sensor to settle...."
    sleep(2)
-   while true do
+   while true do   # infinite loop of doom!
       puts "going to move"
       move_and_check(io)
       find_new_dir(io)
@@ -97,6 +98,3 @@ if direction == "[fwd]" then  #forward
 end
 
 stop_all(io) if direction == "[stp]"
-
-
-stop_all(io)  # too near
